d76ab6dbe35889b6c45ec0bf060c78d1183600e1
price-jamar <c26jamar.price@afacademy.af.edu>
2024-02-12 16:52:01 -0700

    YAY

--- src/hdl/thirtyOneDayMonth.vhd
+++ src/hdl/thirtyOneDayMonth.vhd
@@ -1,4 +1,4 @@
---+----------------------------------------------------------------------------
+-------------------------------------------------------------------------------
 --| 
 --| COPYRIGHT 2017 United States Air Force Academy All rights reserved.
 --| 
@@ -56,9 +56,10 @@ library ieee;
 entity thirtyOneDayMonth is 
   port(
 	i_A : in std_logic; -- one of four inputs
-	
-	
-	
+	i_B : in std_logic;
+    i_C : in std_logic;
+    i_D : in std_logic;
+    o_Y : out std_logic
 						-- output
   );
 end thirtyOneDayMonth;
@@ -67,13 +68,24 @@ architecture thirtyOneDayMonth_arch of thirtyOneDayMonth is
 	-- include components declarations and signals
 	
 	--signals internal to the architecture are declared and initialized such as w_sel
-  
+signal w_sel : std_logic_vector (2 downto 0); -- MUX sel
+
 begin
 	-- CONCURRENT STATEMENTS---------------------------------------
 	--assigning names to reflect original schematics (for ease of understanding if you wish to)
 	w_sel(0) <= i_C;	-- one
 	--finish assigning signals
-	
-	--enter your logic here to implement the mux.  See VHDL reference sheet for MUX syntax.	
+	w_sel(1) <= i_B;
+	w_sel(2) <= i_A;
+	--enter your logic here to implement the mux.  See VHDL reference sheet for MUX syntax.
+	o_Y <= i_D when (w_sel = "000") else
+	       i_D when (w_sel = "001") else
+	       i_D when (w_sel = "010") else
+	       i_D when (w_sel = "011") else
+	       not i_D when (w_sel = "100") else
+	       not i_D when (w_sel = "101") else
+	       not i_D when (w_sel = "110") else
+	       '0';
+	       	
 	---------------------------------------------------------------	
 end thirtyOneDayMonth_arch;
